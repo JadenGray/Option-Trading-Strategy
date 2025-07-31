1510bb59675edf6a6cb8da264e0bc399fc2a85db
Jaden <jaden.silver.gray@gmail.com>
2025-07-31 17:30:03 +0100

    Debug Commit
    Plot title fixed

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import random
 import numpy as np
+import re
 SEED = 100
 random.seed(SEED)
 np.random.seed(SEED)
@@ -20,7 +21,11 @@ trades.to_csv("data/trades.csv", index=False)
 pnl.to_csv("data/daily_pnl.csv", index=False)
 
 # generate and save PnL plot
-fig = plot_pnl(pnl, df['symbol'].iloc[0] if 'symbol' in df.columns else 'SPY', df['option_type'].iloc[0] if 'option_type' in df.columns else 'call', pnl.columns[0])
+# get the strike price for the title
+option_symbol = df.loc[0, 'option_symbol'][2:]
+match = re.search(r'[A-Z]+(\d{6})([CP])(\d{8})$', option_symbol)
+strike_price =int(int(match.group(3)) / 1000)
+fig = plot_pnl(pnl, df['symbol'].iloc[0] if 'symbol' in df.columns else 'SPY', df['option_type'].iloc[0] if 'option_type' in df.columns else 'call', strike_price)
 fig.savefig("plots/pnl_plot.png")
 
 print("Backtest complete, Saved PnL plots and csv files")
